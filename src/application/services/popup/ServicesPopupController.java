25933c1201aff82e5d3e6ef7db720fe9c41732a5
Alin Georgian <alingeorgian@Alins-MacBook-Pro.local>
2022-12-22 19:19:46 +0200

    Added Service Content Skeleton

--- src/application/services/popup/ServicesPopupController.java
+++ src/application/services/popup/ServicesPopupController.java
@@ -1,6 +1,7 @@
 package application.services.popup;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,15 +10,19 @@ import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class ServicesPopupController implements Initializable{
 
@@ -42,74 +47,27 @@ public class ServicesPopupController implements Initializable{
     @FXML
     private GridPane contentPane;
     
-    private int nr = 1;
+    private int nrPressed = 1;
     
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-    	pane = new GridPane();
-    	contentPane = new GridPane();
-    	initializeGridPane(contentPane);
-    	pane.addRow(0, contentPane);
-    	pane.setVgap(10);
-    	scrollPane.setContent(pane);
+    	try {
+    		contentPane = new GridPane();
+			Parent root = FXMLLoader.load(getClass().getResource("/application/services/popup/ServicesContent.fxml"));
+			contentPane.addRow(0, root);
+			scrollPane.setContent(contentPane);
+			scrollPane.setFitToWidth(true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
     
-    private void initializeGridPane(GridPane content) {
-    	TextField serviceName = new TextField();
-    	serviceName.setPrefWidth(200);
-    	serviceName.setPrefHeight(40);
-    	serviceName.setPromptText("Nume Serviciu");
-    	TextField serviceAmount = new TextField();
-    	serviceAmount.setPrefWidth(125);
-    	serviceAmount.setPrefHeight(40);
-    	serviceAmount.setPromptText("Cantitate");
-    	TextField servicePrice = new TextField();
-    	servicePrice.setPrefWidth(200);
-    	servicePrice.setPrefHeight(40);
-    	servicePrice.setPromptText("Pret");
-    	ComboBox<String> serviceExchange = new ComboBox<String>();
-    	serviceExchange.setPrefWidth(125);
-    	serviceExchange.setPrefHeight(40);
-    	serviceExchange.setPromptText("Valuta");
-    	String[] exchangeOptions = {"RON","EUR"};
-    	serviceExchange.getItems().addAll(exchangeOptions);
-    	TextField serviceDescription = new TextField();
-    	serviceDescription.setPrefWidth(200);
-    	serviceDescription.setPrefHeight(40);
-    	serviceDescription.setPromptText("Descriere");
-    	Button clearFields = new Button();
-		FontAwesomeIconView clearFieldsIcon = new FontAwesomeIconView(FontAwesomeIcon.TRASH);
-		clearFieldsIcon.setFill(Color.web("#942c5c"));
-		clearFieldsIcon.setSize("28");
-		clearFields.setGraphic(clearFieldsIcon);
-		clearFields.setOnMouseClicked(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				serviceName.clear();
-				serviceAmount.clear();
-				servicePrice.clear();
-				serviceDescription.clear();
-			}
-		});
-		
-		
-    	content.setHgap(5);
-    	content.setVgap(5);
-    	content.addRow(0, serviceName);
-    	content.addRow(0, serviceAmount);
-    	content.addRow(0, clearFields);
-    	content.addRow(1, servicePrice);
-    	content.addRow(1, serviceExchange);
-    	content.addRow(2, serviceDescription);
-    }
-    
     @FXML
-    void addServiceButtonClicked(ActionEvent event) {
-    	GridPane addedPane = new GridPane();
-    	initializeGridPane(addedPane);
-    	pane.addRow(nr, addedPane);
-    	nr++;
+    void addServiceButtonClicked(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("/application/services/popup/ServicesContent.fxml"));
+    	contentPane.addRow(nrPressed, root);
+    	nrPressed++;
     }
 
     @FXML
