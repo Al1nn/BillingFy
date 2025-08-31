d015d82330252fc904629f9196226f378db816c8
Al1nn <artistulambitios@gmail.com>
2025-08-31 18:52:08 +0300

    Bug fixes

--- generate_invoice_mac.sh
+++ generate_invoice_mac.sh
@@ -17,9 +17,12 @@ pipv="$(pip3 -V 2>&1)"
 if [[ -z "$pipv" ]]
 then
     brew install pip3
+    python3 -m venv .venv
+    source .venv/bin/activate
     pip3 install jinja2
     pip3 install xhtml2pdf
 else
     echo "$pipv"
 fi
+source .venv/bin/activate
 python3 main.py
